21644a11b7b2a9c6f8a652012e2e55a864d2222b
romi9872 <adian281r@gmail.com>
2021-02-22 18:14:32 +0700

    Edit Api Endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('post', 'PostController@index');
 Route::get('post/{id}', 'PostController@show');
 Route::post('post', 'PostController@store');
+Route::put('post/{post}', 'PostController@update');
